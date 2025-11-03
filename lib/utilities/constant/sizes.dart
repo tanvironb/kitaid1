1275b1926778c977a43cddbde0b44026f34813e0
tamvironb <taahmmed123@gmail.com>
2025-11-03 23:40:46 +0800

    latest

---
+++ lib/utilities/constant/sizes.dart
@@ -0,0 +1,60 @@
+class mysizes{
+
+//padding and margin size
+static const double XS = 4.0; // extra small
+static const double sm = 8.0; // small
+static const double nd = 16.0; // medium
+static const double lg = 24.0; //large
+static const double xl = 32.0; //extra large
+
+//Icon size
+static const double iconXS = 12.0; // extra small
+static const double iconsm = 16.0; // small
+static const double iconnd = 24.0; // medium
+static const double iconlg = 32.0; //large
+
+
+//Font size
+static const double fontSm = 12.0; // extra small
+static const double fontMd = 16.0; // small
+static const double fontLg = 24.0; // medium
+
+//button size
+static const double btnheight = 18.0; // extra small
+static const double btnradious = 12.0; // small
+static const double btnweight = 120.0; // medium
+
+//appbar size
+static const double appbarheight = 56.0; // medium
+
+
+
+//default spacing btw sctions
+static const double defaultspace = 24.0; // medium
+static const double spacebtwitems = 16.0; // medium
+static const double spacebtwsections = 32.0; // medium
+
+//border radius
+static const double borderRadiusSm = 4.0; // medium
+static const double borderRadiusMd = 8.0; // medium
+static const double borderRadiusLg = 12.0; // medium
+
+//item dimension
+static const double imageRadius = 16.0; // medium
+
+//input field
+static const double inputfieldRadius = 12.0; // medium
+static const double spacebtwfields = 16.0; // medium
+
+//card sizes
+static const double cardRadiusLg = 16.0; // medium
+static const double cardRadiusMd = 12.0; // medium
+static const double cardRadiusSm = 10.0; // medium
+static const double cardRadiusXs = 6.0; // medium
+
+//grid view spacing
+static const double gridviewspacing = 16.0; // medium
+
+
+
+}
