81e5fafe5563e196824074118f63ee36b5bb05e3
tamvironb <taahmmed123@gmail.com>
2025-11-03 23:36:51 +0800

    latest update

---
+++ lib/utilities/constant/color.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+
+class mycolors{
+
+//primary color
+  static const Color Primary = Color.fromARGB(255, 18, 72, 252);
+
+//text color
+static const Color textPrimary = Color.fromARGB(255, 0, 0, 0);
+static const Color textHeading =Color.fromARGB(255, 18, 72, 252);
+static const Color textinbox = Color.fromARGB(255, 255, 255, 255);
+
+
+//background color
+static const Color borderPrimary = Color.fromARGB(255, 255, 255, 255);
+
+//button color
+static const Color btnPrimary =Color.fromARGB(255, 18, 72, 252);
+static const Color btnSecondary =Color.fromARGB(255, 255, 255, 255);
+
+//border color
+static const Color borderprimary = Color.fromARGB(255, 0, 0, 0);
+
+//error and validation color
+
+//neutral color
+
+}
