7493893c92c5d29c2af5f05ae6b6dabadd1fca8c
Monirsa <monirsa2002@gmail.com>
2025-12-15 01:04:22 +0800

    by monir

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+
+    // ✅ Firebase / Google Services plugin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -20,10 +24,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.kitaid1"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +33,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
-
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:kitaid1/firebase_options.dart';
 import 'package:kitaid1/kitaid.dart';
 
-void main() {
-  runApp(const kitaid());
-  
-}
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  print('🔥 Firebase connected: ${Firebase.apps.first.name}');
+
+  runApp(const kitaid());
+}
 
